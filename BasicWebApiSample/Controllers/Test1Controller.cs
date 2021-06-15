d876d0b3d3b7323151393d0267ca3efa50e8b4e3
Satheesh Reddy Ganji <v-satheg@microsoft.com>
2021-06-15 16:32:00 -0700

    changes

---
+++ BasicWebApiSample/Controllers/Test1Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Microsoft.Examples.Controllers
+{
+    [ApiVersion( "1.0" )]
+    [RoutePrefix( "api/v{version:apiVersion}/test" )]
+    public class Test1Controller : BaseController
+    {
+        [HttpGet]
+        [Route( "GetMethod" )]
+        public string GetMethod( )
+        {
+           return getOne();
+        }
+    }
+}
---
+++ BasicWebApiSample/Controllers/Test2Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Microsoft.Examples.Controllers
+{
+    [ApiVersion( "2.0" )]
+    [RoutePrefix( "api/v{version:apiVersion}/test" )]
+    public class Test2Controller : Base2Controller
+    {
+        [HttpGet]
+        [Route( "GetMethod" )]
+        public string GetMethod()
+        {
+            return getTwo();
+        }
+    }
+}
